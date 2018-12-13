924dac6b58726f3e8326523f25c1cd6485edb92d
chris250299 <tradang.isvnu@gmail.com>
2018-12-13 15:13:51 +0700

    Tìm giá trị lớn nhất trong mảng

---
+++ src/MinValueInArr.java
@@ -0,0 +1,20 @@
+public class MinValueInArr {
+    public static void main(String[] args) {
+        int[] arr = {4, 5, 2, 3, 5, 0};
+        int index = minValue(arr);
+        System.out.println("The smallest element in the array is: " + arr[index]);
+    }
+
+    public static int minValue(int[] array) {
+        int min = array[0];
+        int index = 0;
+        for (int i = 1; i < array.length; i++) {
+            if (array[i] < min) {
+                min = array[i];
+                index = i;
+            }
+        }
+        return index;
+    }
+}
+
